e426fd839354193259b962b9098c7a10adf28894
Chris <caquinn.86@gmail.com>
2019-09-27 19:47:45 -0700

    finished sprint

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import styled from 'styled-components';
+import PersonCard from './components/Person/PersonCard.js';
 import './App.css';
 
 const App = () => {
@@ -8,12 +11,79 @@ const App = () => {
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [page, setPage] = useState(1);
+  const [people, setPeople] = useState([]);
+
+  let pages;
+
+  const getPages = async function() {
+    let res = await axios.get(`https://swapi.co/api/people/?page=1`);
+    pages = Math.ceil(res.data.count / 10);
+  }
+
+  getPages();
+
+  const getPage = async function() {
+    let res = await axios.get(`https://swapi.co/api/people/?page=${page}`);
+    setPeople(res.data.results);
+  }
+
+  useEffect(() => {
+    getPage();
+  }, [page]);
+
+  function changePage(val) {
+    if(val === -1) { // Move back
+      if(page === 1) {
+        setPage(pages);
+      } else {
+        setPage(page - 1);
+      }
+    } else { // Move along
+      if(page === pages) {
+        setPage(1);
+      } else {
+        setPage(page + 1);
+      }
+    }
+  }
+
+  console.log(page);
 
   return (
-    <div className="App">
+    <Div className="App">
       <h1 className="Header">React Wars</h1>
-    </div>
+      <Button onClick={() => changePage(-1)}>Prev</Button>
+      <Button onClick={() => changePage(1)}>Next</Button>
+      <PersonDiv>
+        {people.map(person => {
+          return (
+            <PersonCard
+              key={people.indexOf(person)}
+              name={person.name}
+              birth_year={person.birth_year}
+            />
+          );
+        })}
+      </PersonDiv>
+    </Div>
   );
 }
 
 export default App;
+
+const Div = styled.div`
+  width: 90%;
+  margin: auto;
+`
+
+const PersonDiv = styled.div`
+  margin-top: 4%;
+  display: flex
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-around;
+`
+
+const Button = styled.button`
+`
---
+++ starwars/src/components/Person/PersonCard.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Div = styled.div`
+    background: white;
+    border: 1px solid black;
+    opacity: 0.75;
+    width: 40%;
+    margin: 1% 4% 1% 4%;
+    height: 100px;
+`
+
+export default function PersonCard(props) {
+    return (
+        <Div>
+            <h3>{props.name}</h3>
+            <p>Birth Year: {props.birth_year}</p>
+        </Div>
+    );
+}
